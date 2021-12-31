b0a14991726cd7a683cdb6f7b6be20c6f2285579
sunbor <44109837+sunbor@users.noreply.github.com>
2021-12-30 20:59:33 -0600

    basic database access

---
+++ crudserver.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql2');
+const propertiesReader = require('properties-reader');
+
+const app = express();
+
+const PORT = 3000;
+
+const properties = propertiesReader('database.properties');
+
+const connection = mysql.createConnection({
+    host: properties.get("host"),
+    user: properties.get("user"),
+    password: properties.get("password"),
+    database: properties.get("database")
+})
+
+connection.connect(error => {
+    if(error){
+        console.log("oh no!!");
+        throw error;
+    }
+
+    app.listen(PORT, () => {
+        console.log("connected to server on port:", PORT);
+    })
+})
+
+connection.query('SELECT * FROM users', (err, res, fields) => {
+    console.log(res[0]);
+});
+
+// app.listen(3000, () => {
+//     console.log("starting the crud server");
+// })
+
+// app.get('/', (req, res) => {
+//     res.send("hello this is the crud server");
+// })
