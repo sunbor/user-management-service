010ba435511daec89888c784757bdb91168e2488
sunbor <44109837+sunbor@users.noreply.github.com>
2022-01-01 00:58:07 -0600

    added endpoints for basic crud functions

--- crudserver.js
+++ crudserver.js
@@ -2,6 +2,7 @@ const express = require('express');
 const res = require('express/lib/response');
 const mysql = require('mysql2/promise');
 const propertiesReader = require('properties-reader');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -16,28 +17,76 @@ const connProps = {
     database: properties.get("database")
 };
 
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
 app.listen(PORT, () => {
     console.log("starting the crud server");
 })
 
 app.get('/users', async function(req, res) {
-
-    const result = await sqlQuery('SELECT * FROM users');
-    res.send(result);
-
+    try{
+        const result = await sqlQuery('SELECT * FROM users');
+        res.send(result);
+    }
+    catch(error){
+        console.log(error);
+        res.send("failed to get users");
+    }
 })
 
 app.get('/users/:userId', async function(req, res) {
-    console.log(req.params.userId);
-    const result = await sqlQuery('SELECT * FROM users WHERE userId = ?', [req.params.userId]);
-    res.send(result);
-
+    try{
+        const result = await sqlQuery('SELECT * FROM users WHERE userId = ?', [req.params.userId]);
+        res.send(result);
+    }
+    catch(error){
+        console.log(error);
+        res.send("failed to find user with id ${req.params.userId}");
+    }
 })
 
+app.post('/users', async function(req, res) {
+    try{
+        const result = await sqlQuery('INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, ?)', 
+        [req.body.username, req.body.password, req.body.email, req.body.role]);
+        res.send(result);
+    }
+    catch(error){
+        console.log(error);
+        res.send("failed to create new user");
+    }
+})
 
+app.put('/users/:userId', async function(req, res) {
+    try{
+        const result = await sqlQuery('UPDATE users SET username = ?, password = ?, email = ?, role = ? WHERE userid = ?;', 
+        [req.body.username, req.body.password, req.body.email, req.body.role, req.params.userId]);
+        res.send(result);
+    }
+    catch(error){
+        console.log(error);
+        res.send("failed to update user with id ${req.params.userId}");
+    }
+})
+
+app.delete('/users/:userId', async function(req, res) {
+    try{
+        const result = await sqlQuery('DELETE FROM users WHERE userId = ?', [req.params.userId]);
+        res.send(result);
+    }
+    catch(error){
+        console.log(error);
+        res.send("failed to delete user with id ${req.params.userId}");
+    }
+})
 
 async function sqlQuery(cmd, params){
-    console.log(params);
+    //console.log(params);
     const dbConnection = await mysql.createConnection(connProps);
     const [rows] = await dbConnection.execute(cmd, params);
     
